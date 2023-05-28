30197e04f4500a824f6ea08336f3fda6079afde5
udikakumari2 <udikakumari5@gmail.com>
2023-05-28 10:15:22 -0700

    add usestate

--- project04/src/App.js
+++ project04/src/App.js
@@ -4,26 +4,42 @@ import HomeOne from './pages/HomeOne';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
+import { useState } from 'react';
 
 
 
 function App() {
+  //let  count=0;
+  const [count,setCount]=useState(0);
+  const increment = () =>{
+    setCount(count+1);
+    //count++
+  };
+  const decrement = () =>{
+    setCount(count-1);
+  };
   return (
-    <Router>
-      <div>
-       <Routes>
-          <Route exact path="/"element={<HomeOne/>}>
-         
-          </Route>
-          <Route path="/About"element={<About/>}>
-         
-          </Route>
-          <Route path="/ContactUs"element={<ContactUs/>}>
-         
-          </Route>
-        </Routes> 
-      </div>
-    </Router>
+    <>
+    <h1 class="title">My counter</h1>
+    <span class="subtitle">Count is {count}</span>
+    <button onClick={decrement}class="button">-</button>
+    <button onClick={increment}class="button">+</button></>
+    /* <Router>
+    <div>
+     <Routes>
+        <Route exact path="/"element={<HomeOne/>}>
+       
+        </Route>
+        <Route path="/About"element={<About/>}>
+       
+        </Route>
+        <Route path="/ContactUs"element={<ContactUs/>}>
+       
+        </Route>
+      </Routes> 
+    </div>
+  </Router>
+  <> */
   );
 }
 
