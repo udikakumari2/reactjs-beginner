303113f5ff2e6b550985ad6c9f818501ba2476a4
udikakumari2 <90750502+udikakumari2@users.noreply.github.com>
2023-07-13 14:33:18 +0530

    add

--- MyApplicationbhn/app/src/main/java/com/example/myapplicationbhn/MainActivity.java
+++ MyApplicationbhn/app/src/main/java/com/example/myapplicationbhn/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText num1;
     private EditText num2;
-    private Button add;
+    private Button add,sub1;
     private TextView answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +20,27 @@ public class MainActivity extends AppCompatActivity {
         num1=(EditText) findViewById(R.id.n1);
         num2=(EditText) findViewById(R.id.n2);
        add=(Button) findViewById(R.id.btn);
+        sub1=(Button) findViewById(R.id.btn1);
         answer=(TextView) findViewById(R.id.txt1);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    answer.setText(num1.+num2);
+                   int number1=Integer.parseInt(num1.getText().toString());
+                int number2=Integer.parseInt(num2.getText().toString());
+                int sum=number1+number2;
+
+                answer.setText("Answer is : "+sum);
+
+            }
+
+        });
+        sub1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number1=Integer.parseInt(num1.getText().toString());
+                int number2=Integer.parseInt(num2.getText().toString());
+                int sub2=number1-number2;
+                answer.setText("Substraction is : "+sub2);
             }
         });
     }
