344ced7a3257882d278226becf930a7b78e7443b
udikakumari2 <90750502+udikakumari2@users.noreply.github.com>
2023-07-13 12:36:07 +0530

    addd

--- MyApplicationft/app/src/main/java/com/example/myapplicationft/MainActivity.java
+++ MyApplicationft/app/src/main/java/com/example/myapplicationft/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.myapplicationft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import android.widget.TextView;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView username=(TextView) findViewById(R.id.username);
+        TextView password=(TextView) findViewById(R.id.password);
+        MaterialButton loginbtn=(MaterialButton) findViewById(R.id.login);
+        //admin
+      loginbtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+             if(username.getText().toS)
+          }
+      });
+
+
+
 
 
     }
