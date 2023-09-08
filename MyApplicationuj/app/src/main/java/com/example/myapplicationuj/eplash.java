8535031faf1f510ea25d9bd4a5abc763c5b08cb0
udikakumari2 <90750502+udikakumari2@users.noreply.github.com>
2023-09-08 16:26:02 +0530

    dfx

---
+++ MyApplicationuj/app/src/main/java/com/example/myapplicationuj/eplash.java
@@ -0,0 +1,42 @@
+package com.example.myapplicationuj;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.SmsManager;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash extends AppCompatActivity {
+    ImageView logo;
+    TextView name, own1, own2;
+    Animation topAnim, bottomAnim;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eplash);
+       
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
